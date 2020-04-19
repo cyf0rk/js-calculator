6be5cf6fe9edd102a4d4b728e9a282349b99cd57
marijan111 <marijanbao@gmail.com>
2020-04-19 17:35:03 +0200

    finish

--- javascript/animations.js
+++ javascript/animations.js
@@ -1,43 +1,47 @@
-let buttons = $('.numbers div')
+// jQuery DOM selector for easier read and use in the code
+const buttons = $('.numbers div')
   .add('.operators div')
   .add('#clear')
   .add('.equal');
 
+// jQuery on mousedown function will make buttons appear as clicked, switch statement for easier read
 $(buttons).on('mousedown', (el) => {
-  if ($(el.target).is('.numbers div')) {
-    $(el.target).css('border-color', '#b0b0b0');
-    $(el.target).css('box-shadow', '0 1px 1px 0 #000');
-  }
-
-  if ($(el.target).is('.operators div')) {
-    $(el.target).css('border-color', '#4d4d4d');
-    $(el.target).css('box-shadow', '0 1px 1px 0 #000');
-  }
-
-  if ($(el.target).is('#clear')) {
-    $(el.target).css('border-color', '#9c0000');
-    $(el.target).css('box-shadow', '0 1px 1px 0 #000');
-  }
-
-  if ($(el.target).is('.equal')) {
-    $(el.target).css('border-color', '#dedede');
-    $(el.target).css('box-shadow', '0 1px 1px 0 #000');
+  // Checks which DOM element is clicked (true) and changes css properties for that element
+  switch (true) {
+    case $(el.target).is('.numbers div'):
+      $(el.target).css('border-color', '#b0b0b0');
+      $(el.target).css('box-shadow', '0 1px 1px 0 #000');
+      break;
+    case $(el.target).is('.operators div'):
+      $(el.target).css('border-color', '#4d4d4d');
+      $(el.target).css('box-shadow', '0 1px 1px 0 #000');
+      break;
+    case $(el.target).is('#clear'):
+      $(el.target).css('border-color', '#9c0000');
+      $(el.target).css('box-shadow', '0 1px 1px 0 #000');
+      break;
+    case $(el.target).is('.equal'):
+      $(el.target).css('border-color', '#dedede');
+      $(el.target).css('box-shadow', '0 1px 1px 0 #000');
+      break;
   }
 });
 
+// jQuery on mouseup function will make button appear as released after click
 $(buttons).on('mouseup', (el) => {
-  if ($(el.target).is('.numbers div, .operators div')) {
-    $(el.target).css('border-color', '#000');
-    $(el.target).css('box-shadow', '');
-  }
-
-  if ($(el.target).is('#clear')) {
-    $(el.target).css('border-color', '#000');
-    $(el.target).css('box-shadow', '');
-  }
-
-  if ($(el.target).is('.equal')) {
-    $(el.target).css('border-color', '#808080');
-    $(el.target).css('box-shadow', '');
+  // Checks which DOM element is clicked (true) and changes css properties for that element
+  switch (true) {
+    case $(el.target).is('.numbers div, .operators div'):
+      $(el.target).css('border-color', '#000');
+      $(el.target).css('box-shadow', '');
+      break;
+    case $(el.target).is('#clear'):
+      $(el.target).css('border-color', '#000');
+      $(el.target).css('box-shadow', '');
+      break;
+    case $(el.target).is('.equal'):
+      $(el.target).css('border-color', '#808080');
+      $(el.target).css('box-shadow', '');
+      break;
   }
 });
--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,110 @@
-let calScreen = document.querySelector('#calcScreen');
+const calScreen = document.querySelector('#cal-screen');
+const numbers = document.querySelectorAll('.numbers div');
+const operators = document.querySelectorAll('.operators div:not(:last-child)');
+const result = document.querySelector('#result');
+const clear = document.querySelector('#clear');
+const remove = document.querySelector('#remove');
+
+// An array will store all button values
+let selectionArr = [];
+// String that will store math operations
+let valueStr = '';
+
+// Functions that creates all event listeners
+function eventListeners() {
+  let numbersBtn = Array.from(numbers);
+  let operatorsBtn = Array.from(operators);
+
+  numbersBtn.forEach((button) => {
+    button.addEventListener('click', operations.addVal);
+  });
+
+  operatorsBtn.forEach((button) => {
+    button.addEventListener('click', operations.addVal);
+  });
+
+  result.addEventListener('click', operations.calculate);
+  clear.addEventListener('click', operations.clear);
+  remove.addEventListener('click', operations.remove);
+}
+
+// This function will check if provided last index is number or not
+function checkValueType(value, lastIndex) {
+  if (isNaN(lastIndex)) {
+    // If last index is operator it will replace that operator with one that is clicked
+    switch (value) {
+      case '\327':
+        selectionArr.splice(selectionArr.length - 1, 1, '*');
+        break;
+      case '\367':
+        selectionArr.splice(selectionArr.length - 1, 1, '/');
+        break;
+      case '+':
+        selectionArr.splice(selectionArr.length - 1, 1, '+');
+        break;
+      case '-':
+        selectionArr.splice(selectionArr.length - 1, 1, '-');
+        break;
+      case '%':
+        selectionArr.splice(selectionArr.length - 1, 1, '%');
+        break;
+    }
+  } else if (!isNaN(lastIndex)) {
+    // If last index is not operator statement will add clicked operator
+    switch (value) {
+      case '\327':
+        selectionArr.push('*');
+        break;
+      case '\367':
+        selectionArr.push('/');
+        break;
+      case '+':
+        selectionArr.push('+');
+        break;
+      case '-':
+        selectionArr.push('-');
+        break;
+      case '%':
+        selectionArr.push('%');
+        break;
+    }
+  }
+}
+
+// Object with methods that are executed on button clicks
+const operations = {
+  // Push value of clicked element to selectionArr and add number or operator to calculator screen
+  addVal(el) {
+    elValue = el.target.innerHTML;
+    let lastIndex = selectionArr[selectionArr.length - 1];
+    if (isNaN(elValue)) {
+      checkValueType(elValue, lastIndex);
+    } else {
+      selectionArr.push(elValue);
+    }
+    calScreen.value = selectionArr.join('').toString();
+  },
+  // Removes last item of selectionArr array
+  remove() {
+    selectionArr.pop();
+    calScreen.value = selectionArr.join('').toString();
+  },
+  // Clears all inputs
+  clear() {
+    selectionArr = [];
+    valueStr = '';
+    calScreen.value = 0;
+  },
+  // Calculates math operations in valueStr string and displays it to calculator screen
+  calculate() {
+    selectionArr.forEach((value) => {
+      valueStr += value;
+    });
+    calScreen.value = eval(valueStr);
+    selectionArr = [];
+    valueStr = '';
+  },
+};
+
+// Calling all event listeners so that we can use button events
+eventListeners();
